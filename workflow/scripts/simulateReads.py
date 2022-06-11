334870cad884a8e97028999bbeba7094545928a1
Your Name <etholleman@ucdavis.edu>
2022-06-11 11:13:10 -0700

    add functions for making chr and reads

--- workflow/scripts/simulateReads.py
+++ workflow/scripts/simulateReads.py
@@ -5,7 +5,10 @@
 
 import numpy as np
 from Bio import SeqIO
-
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+import pandas as pd
+from pathlib import Path
 
 class chromosomeGenerator():
     
@@ -16,6 +19,7 @@ class chromosomeGenerator():
         self.c_content = c_content
         self.c_spacing = c_spacing
         self.seq = ['N'] * self.length
+        self.sub_id = 0
     
     @property
     def spacing_method(self):
@@ -47,14 +51,24 @@ class chromosomeGenerator():
         return int(self.length * self.c_content)
     
     
-    def generate_sequence(self):
+    def get_sequence(self):
         '''Generate final sequence for this chromosome, store in `seq` attribute
         '''
-        # Place C bases according to specified spacing method
+        # Check if previous sequence has already been generated. If True
+        # then delete it
+        if 'C' in self.seq:
+            self.seq = ['N'] * self.length
+        
         self.spacing_method()
         assert 'C' in self.seq
         self._fill_non_c_bases()
         assert 'N' not in self.seq
+        self.sub_id += 1
+    
+    
+    def __next__(self):
+        self.get_sequence()
+        return self._to_seq_record()
 
     
     def _even_c(self):
@@ -104,9 +118,148 @@ class chromosomeGenerator():
             if base != 'C':
                 base = np.random.choice(['A', 'T', 'G'])
                 self.seq[i] = base
+    
+    
+    def _to_seq_record(self):
+        record = SeqRecord(
+            Seq(''.join(self.seq)),
+            id=f'c{self.chromosome_id}-{self.sub_id}',
+            name=self.nickname,
+            description='Random chromosome C content:{self.c_content} with {self.c_spacing} distribution'
+        )
+        return record
 
 
+# class readGenerator():
+    
+#     def __init__(self, sequence, )
+
+c = chromosomeGenerator(1, 'test', 2000, 0.21, 'sin')
+
+
+    
 class readGenerator():
     
-    def __init__(self, sequence, )
+    
+    def __init__(self, seq_record, conversion_eff, avg_loop_length=200, loop_length_sd=20):
+        self.seq_record = seq_record
+        self.conversion_eff = conversion_eff
+        self.read_length = read_length
+        self.avg_loop_length = avg_loop_length
+        self.read_counter = 0
+        self.conversion_track = []  # 2=Converted C  # 1=Unconverted C # 0=Non-C base
+    
+    
+    def get_read(self):
+        length = np.random.normal(loc=avg_loop_length, scale=loop_length_sd)
+        start_pos = np.random.randint(0, len(seq_record.seq)-length)
+        read_bases = self.seq_record.seq[start_pos:start_pos+length]
+        converted_read = ['N'] * len(read_bases)
+        
+        for each_base in read_bases:
+            if 'C' == each_base:
+                if np.random.random_sample <= conversion_eff:
+                    converted_read.append('T')  # C->T conversion occurs
+                    self.conversion_track.append('2')
+                    continue
+                else:
+                    self.conversion_track.append('1')
+                    continue
+            converted_read.append(each_base)
+            self.conversion_track.append('0')
+        
+        self.read_counter += 1
+        
+        return SeqRecord(
+            Seq(''.join(converted_read)),
+            id=f'{seq_record.id}+r{self.read_counter}',
+            name=f'{seq_record.name} read {self.read_counter}',
+            annotations={
+                'conversion_eff': self.conversion_eff,
+                'read_length': length,
+                'read_id': self.read_counter,
+                'read_start': self.start_pos,
+                'avg_loop_length': self.average_loop_length,
+                'chr_description': self.seq_record.description,
+                'chr_id': self.seq_record.id,
+                'chr_name': self.seq_record.name,
+                'read_seq': ''.join(converted_read),
+                'conversion_track': ''.join(self.conversion_track)
+            },
+            letter_annotations={
+                'solexa_quality':[40] * length
+            }
+        )
+    
+    def __next__(self):
+        return self.get_read()
+
+
+
+
+def make_chromosomes(chromosome_table, chr_output_dir, read_output_dir, record_path):
+    read_records = []
+    for index, row in chromosome_table:
+        # Prepare generator to make chromosomes with characteristics specified
+        # by current row of chromosome table
+        generator = chromosomeGenerator(
+            row['chromosome_id'], row['nickname'], row['length'], 
+            row['c_content'], row['c_spacing']
+        )
+        for i in row['num_copies']:
+            current_chrom = next(generator)
+            fasta_name = f'CHR-{current_chrom.id}.fa'
+            fasta_path = Path(chr_output_dir).joinpath(fasta_name)
+            # write generated chromosome to fasta file
+            with open(str(fasta_path), 'w') as handle:
+                SeqIO.write([current_chrom], handle, 'fasta')
+            
+            # generate reads from chromosome 
+            read_gen = readGenerator(current_chrom, row['conversion_eff'])
+            reads = [next(read_gen) for _ in row['num_reads']]
+            
+            # Set location for writing reads to
+            fastq_name = f'READS-{current_chrom.id}.fastq'
+            fastq_path = Path(read_output_dir).joinpath(fastq_name)
+            
+            # record read information in table for later reference by other
+            # programs and easier data wrangling when plotting
+            for read in reads:
+                read.annotations['fastq_path'] = str(fastq_path)
+                read.annotations['chr_fasta_path'] = str(fasta_path)
+                read_records.append(read.annotations)
+            
+            # write reads to fastq file
+            with open(str(fastq_path), 'w') as handle:
+                SeqIO.write(reads, handle, 'fastq')
+    
+    # At this point all reads and chromosomes should have been written
+    # and recorded in read_records, now just need to save read_records as
+    # a tsv file for later use.
+    
+    pd.DataFrame(read_records).to_csv(record_path, index=False, sep='\t')
+    
+    return record_path
+            
+    
                 
+            
+            
+        
+        
+        
+
+
+def main():
+    
+    chromosome_table = pd.read(args.chr_table)
+    # should have chromosome characteristics and how many to generate
+    
+        
+
+                    
+            
+        
+        
+    
+    
